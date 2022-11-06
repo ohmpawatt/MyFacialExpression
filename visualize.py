375fb679b9ac859a34919138be0d2a49a05991d5
ohmpawatt <yjq15270932099@163.com>
2022-11-06 20:40:27 +0800

    update visualize

--- visualize.py
+++ visualize.py
@@ -9,6 +9,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import os
+
+from ui.ui import UI
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from torch.autograd import Variable
 
 import transforms as transforms
@@ -23,12 +27,14 @@ transform_test = transforms.Compose([
     transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
 ])
 
+
 def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
 
 raw_img = io.imread('images/1.jpg')
 gray = rgb2gray(raw_img)
-gray = resize(gray, (48,48), mode='symmetric').astype(np.uint8)
+gray = resize(gray, (48, 48), mode='symmetric').astype(np.uint8)
 
 img = gray[:, :, np.newaxis]
 
@@ -48,37 +54,36 @@ ncrops, c, h, w = np.shape(inputs)
 
 inputs = inputs.view(-1, c, h, w)
 inputs = inputs.cuda()
-inputs = Variable(inputs, volatile=True)
+with torch.no_grad():
+    inputs = Variable(inputs)
 outputs = net(inputs)
-
 outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
 
-score = F.softmax(outputs_avg)
+score = F.softmax(outputs_avg, dim=-1)
 _, predicted = torch.max(outputs_avg.data, 0)
 
-plt.rcParams['figure.figsize'] = (13.5,5.5)
-axes=plt.subplot(1, 3, 1)
+plt.rcParams['figure.figsize'] = (13.5, 5.5)
+axes = plt.subplot(1, 3, 1)
 plt.imshow(raw_img)
 plt.xlabel('Input Image', fontsize=16)
 axes.set_xticks([])
 axes.set_yticks([])
 plt.tight_layout()
 
-
 plt.subplots_adjust(left=0.05, bottom=0.2, right=0.95, top=0.9, hspace=0.02, wspace=0.3)
 
 plt.subplot(1, 3, 2)
-ind = 0.1+0.6*np.arange(len(class_names))    # the x locations for the groups
-width = 0.4       # the width of the bars: can also be len(x) sequence
-color_list = ['red','orangered','darkorange','limegreen','darkgreen','royalblue','navy']
+ind = 0.1 + 0.6 * np.arange(len(class_names))  # the x locations for the groups
+width = 0.4  # the width of the bars: can also be len(x) sequence
+color_list = ['red', 'orangered', 'darkorange', 'limegreen', 'darkgreen', 'royalblue', 'navy']
 for i in range(len(class_names)):
     plt.bar(ind[i], score.data.cpu().numpy()[i], width, color=color_list[i])
-plt.title("Classification results ",fontsize=20)
-plt.xlabel(" Expression Category ",fontsize=16)
-plt.ylabel(" Classification Score ",fontsize=16)
+plt.title("Classification results ", fontsize=20)
+plt.xlabel(" Expression Category ", fontsize=16)
+plt.ylabel(" Classification Score ", fontsize=16)
 plt.xticks(ind, class_names, rotation=45, fontsize=14)
 
-axes=plt.subplot(1, 3, 3)
+axes = plt.subplot(1, 3, 3)
 emojis_img = io.imread('images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())]))
 plt.imshow(emojis_img)
 plt.xlabel('Emoji Expression', fontsize=16)
@@ -87,10 +92,8 @@ axes.set_yticks([])
 plt.tight_layout()
 # show emojis
 
-#plt.show()
+# plt.show()
 plt.savefig(os.path.join('images/results/l.png'))
 plt.close()
 
-print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
-
-
+print("The Expression is %s" % str(class_names[int(predicted.cpu().numpy())]))
